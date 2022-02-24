df77a720e8220ea1e4ea1bc130b9273edd395bbb
tubaargin <tubaargin1995@gmail.com>
2022-02-24 21:17:13 +0300

    Simple Calculator

---
+++ app/src/main/java/com/tubaargin/simplecalculator/MainActivity.java
@@ -0,0 +1,85 @@
+package com.tubaargin.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText number1Text;
+    EditText number2Text;
+    TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number1Text = findViewById(R.id.number1Text);
+        number2Text = findViewById(R.id.number2Text);
+        resultText = findViewById(R.id.resultText);
+
+    }
+
+    public void sum(View view) {
+        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")) {
+            resultText.setText("Enter number");
+
+        } else {
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+            int result = number1 + number2;
+            resultText.setText("Result:" + result); //result yazamayız setText'de integerı stringe çevirmemiz gerekiyor başında metin varsa bizim için stringe çeviriyor
+        }
+
+    }
+
+    public void deduct(View view) {
+        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")) {
+            resultText.setText("Enter number");
+
+        } else {
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+            int result = number1 - number2;
+            resultText.setText("Result:" + result);
+        }
+
+    }
+
+
+    public void multiply(View view) {
+        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")) {
+            resultText.setText("Enter number");
+
+        } else {
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+            int result = number1 * number2;
+            resultText.setText("Result:" + result);
+        }
+
+    }
+
+
+    public void divide(View view) {
+        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")) {
+            resultText.setText("Enter number");
+
+        } else {
+            float number1 = Integer.parseInt(number1Text.getText().toString());
+            float number2 = Integer.parseInt(number2Text.getText().toString());
+            float result = number1 / number2;
+            resultText.setText("Result:" + result);
+        }
+    }
+
+    public void delete(View view) {
+        number1Text.setText(" ");
+        number2Text.setText(" ");
+
+    }
+
+}
